d68d90d0451ee3cea585ae9f7e1ac9c9d0cb18a7
Tejasvi <tejasvi.22111066@viit.ac.in>
2024-02-04 18:26:58 +0530

    commit

---
+++ app.py
@@ -0,0 +1,80 @@
+import streamlit as st
+import pickle
+import numpy as np
+import pandas as pd
+
+
+
+
+
+# Load the model and DataFrame
+pipe = pickle.load(open('pipe.pkl','rb'))
+df = pd.read_pickle('df.pkl')
+
+# Save the DataFrame again
+df.to_pickle('df.pkl')
+
+
+st.title("CashCraft")
+
+# brand
+company = st.selectbox('Brand', df['Company'].unique())
+
+# type of laptop
+laptop_type = st.selectbox('Type', df['TypeName'].unique())
+
+# Ram
+ram = st.selectbox('RAM(in GB)', [2, 4, 6, 8, 12, 16, 24, 32, 64])
+
+# weight
+weight = st.number_input('Weight of the Laptop')
+
+# Touchscreen
+touchscreen = st.selectbox('Touchscreen', ['No', 'Yes'])
+
+# IPS
+ips = st.selectbox('IPS', ['No', 'Yes'])
+
+# screen size
+screen_size = st.number_input('Screen Size')
+
+# resolution
+resolution = st.selectbox('Screen Resolution', ['1920x1080', '1366x768', '1600x900', '3840x2160', '3200x1800', '2880x1800', '2560x1600', '2560x1440', '2304x1440'])
+
+# CPU
+cpu = st.selectbox('CPU', df['Cpu brand'].unique())
+
+hdd = st.selectbox('HDD(in GB)', [0, 128, 256, 512, 1024, 2048])
+
+ssd = st.selectbox('SSD(in GB)', [0, 8, 128, 256, 512, 1024])
+
+gpu = st.selectbox('GPU', df['Gpu brand'].unique())
+
+os = st.selectbox('OS', df['os'].unique())
+
+if st.button('Predict Price'):
+    # query
+    ppi = None
+    if touchscreen == 'Yes':
+        touchscreen = 1
+    else:
+        touchscreen = 0
+
+    if ips == 'Yes':
+        ips = 1
+    else:
+        ips = 0
+
+    # Avoid ZeroDivisionError
+    if screen_size != 0:
+        X_res = int(resolution.split('x')[0])
+        Y_res = int(resolution.split('x')[1])
+        ppi = ((X_res ** 2) + (Y_res ** 2)) ** 0.5 / screen_size
+
+    query = pd.DataFrame(data=[[company, laptop_type, ram, weight, touchscreen, ips, ppi, cpu, hdd, ssd, gpu, os]], 
+                          columns=['Company', 'TypeName', 'Ram', 'Weight', 'Touchscreen', 'Ips', 'ppi', 'Cpu brand', 'HDD', 'SSD', 'Gpu brand', 'os'])
+
+    predicted_price = int(np.exp(pipe.predict(query)[0]))
+
+    # Display the result
+    st.title("The predicted price of this configuration is $" + str(predicted_price))
